17d60d7d9de75eec3c31fe6d4a6c28a95119e160
Daniel Gromak <daniel.gromak@bluesoft.com>
2026-06-13 10:16:06 +0200

    Redeploy API to include google-auth routes

---
+++ tmp_build_push.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+set -e
+cd /mnt/c/FFApi/my_fastapi_project
+TAG='20260606150810'
+echo "[$(date +%FT%T)] TAG=$TAG start login"
+echo 'BSXX8AsGDU3hdrKDSYTmwvW7giL1wrfcEYOO7OCMW52QWdP6AvmXJQQJ99CFACE1PydEqg7NAAACAZCRBEbY' | sudo docker login zapieappapiacrx.azurecr.io --username zapieappapiacrx --password-stdin
+echo "[$(date +%FT%T)] start build"
+sudo docker build -t zapieappapiacrx.azurecr.io/zapieapp-api:$TAG -t zapieappapiacrx.azurecr.io/zapieapp-api:latest .
+echo "[$(date +%FT%T)] build done"
+sudo docker push zapieappapiacrx.azurecr.io/zapieapp-api:$TAG
+sudo docker push zapieappapiacrx.azurecr.io/zapieapp-api:latest
+echo "[$(date +%FT%T)] push done"
---
+++ tmp_check_conn.py
@@ -0,0 +1,12 @@
+﻿import traceback
+from sqlalchemy import create_engine
+
+cs = 'mssql+pyodbc://zapieAdmin:Sukiparabuki1!@zapieapp.database.windows.net:1433/zapieapp?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=no&loginTimeout=1200'
+print('cs=',cs)
+engine = create_engine(cs)
+try:
+    with engine.connect() as conn:
+        row = conn.exec_driver_sql('SELECT 1').fetchone()
+        print('OK', row)
+except Exception:
+    traceback.print_exc()
